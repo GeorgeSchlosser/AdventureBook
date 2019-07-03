91f9db93bb0b7ce2c81acfca39d9e5ee609c9c04
NChislom <11775893+nchislom@users.noreply.github.com>
2019-07-02 20:41:23 -0400

    Replaced minified chooseit with regular version

--- public/choose-it.js
+++ public/choose-it.js
@@ -1,1 +1,35 @@
-!function(a){"use strict";a.fn.chooseIt=function(b){var c=a.extend({initSlide:"slide-1",navClass:"ci-nav",slideClass:"ci-slide"},b),d=function(){var b=a(this);b.parent("."+c.slideClass).fadeOut("fast",function(){a(this).siblings("#"+b.attr("path")).fadeIn("slow")})};return this.each(function(){var b=a(this);b.children("."+c.slideClass).each(function(){a(this).children("."+c.navClass).click(d)}).hide(),b.children("#"+c.initSlide).fadeIn("slow")})}}(jQuery);
+(function($) {
+    'use strict';
+    // Get user location via HTTP GET Request res.body.currentLocation
+    var currentLocation = 'slide-1';
+
+    $.fn.chooseIt = function( options ) {
+
+        var settings = $.extend({
+            initSlide: currentLocation,
+            navClass: 'ci-nav',
+            slideClass: 'ci-slide'
+        }, options);
+
+        var showNextSlide = function() {
+
+            var action = $(this);
+            console.log(action.attr('path')); // This sends to server in a POST request
+    
+            action.parent('.' + settings.slideClass).fadeOut('fast', function() {
+                $(this).siblings('#' + action.attr('path')).fadeIn('slow');
+            });
+        };
+
+        return this.each(function() {
+
+            var thisSlide = $(this);
+
+            thisSlide.children('.' + settings.slideClass).each(function() {
+                $(this).children('.' + settings.navClass).click( showNextSlide );
+            }).hide();
+
+            thisSlide.children('#' + settings.initSlide).fadeIn('slow');
+        });
+    };
+})(jQuery);
